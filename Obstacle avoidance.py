748f19262c05ce63efc710365a13b973db182f99
matsuyamayukihiro <89179423+matsuyamayukihiro@users.noreply.github.com>
2021-09-30 18:28:42 +0900

    realsense2台仕様 左右指示機能あり20210930

--- Obstacle avoidance.py
+++ Obstacle avoidance.py
@@ -32,7 +32,7 @@ if device_product_line == 'L500':
 else:
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-config.enable_device('912112073474')
+config.enable_device('838212070171')
 # Start streaming
 profile = pipeline.start(config)
 
@@ -43,7 +43,7 @@ print("Depth Scale is: ", depth_scale)
 
 # We will be removing the background of objects more than
 #  clipping_distance_in_meters meters away
-clipping_distance_in_meters = 2.5  # 排除する距離パラメータ[m]
+clipping_distance_in_meters = 5.5  # 排除する距離パラメータ[m]
 clipping_distance = clipping_distance_in_meters / depth_scale  # depth基準作り
 
 # Create an align object
@@ -81,7 +81,7 @@ if device_product_line1 == 'L500':
 else:
     config1.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-config1.enable_device('838212070171')
+config1.enable_device('912112073474')
 # Start streaming
 profile1 = pipeline1.start(config1)
 
@@ -92,7 +92,6 @@ print("Depth Scale is: ", depth_scale1)
 
 # We will be removing the background of objects more than
 #  clipping_distance_in_meters meters away
-clipping_distance_in_meters = 2.5  # 排除する距離パラメータ[m]
 clipping_distance1 = clipping_distance_in_meters / depth_scale1  # depth基準作り
 
 # Create an align object
@@ -135,7 +134,7 @@ try:
 
         # 画質変更
         #   h = 320
-        w = 480
+        #   w = 480
         #   dst_R = cv2.resize(color_image_R, dsize=(w, h))
         #   dst_L = cv2.resize(color_image_L, dsize=(w, h))
         #   dst_depth_R = cv2.resize(depth_image_R, dsize=(w, h))
@@ -144,63 +143,84 @@ try:
         #   depth_gry_L = cv2.resize(depth_colormap_L, dsize=(w, h))
 
         # ぼかし加工。
-        #   for i in range(2):
-        #       average_square_size = 10  # ぼかしパラメータ 大きくする程にぼけていくdef=15
-        #       sigma_color = 5000  # 色空間に関する標準偏差パラメータ  大きくすると色の平滑化範囲を広げるdef=5000
-        #       sigma_metric = 1  # 距離空間に関する標準偏差パラメータ  大きくすると色の平滑化範囲を広げる (d==0の時のみ有効)
-        #       img_bilateral_R = cv2.bilateralFilter(dst_R, average_square_size, sigma_color,
-        #                                             sigma_metric)  # Bilateralオペレータを使用して平滑化
-        #       img_bilateral_L = cv2.bilateralFilter(dst_L, average_square_size, sigma_color,
-        #                                             sigma_metric)  # Bilateralオペレータを使用して平滑化
-        #       hsv_img_R = cv2.cvtColor(img_bilateral_R, cv2.COLOR_BGR2HSV)  # HSVモデルに変更
-        #       img_gly_R = cv2.cvtColor(dst_R, cv2.COLOR_BGR2GRAY)  # グレースケール
-        #       hsv_img_L = cv2.cvtColor(img_bilateral_L, cv2.COLOR_BGR2HSV)  # HSVモデルに変更
-        #       img_gly_L = cv2.cvtColor(dst_L, cv2.COLOR_BGR2GRAY)  # グレースケール
+        for i in range(2):
+            average_square_size = 10  # ぼかしパラメータ 大きくする程にぼけていくdef=15
+            sigma_color = 5000  # 色空間に関する標準偏差パラメータ  大きくすると色の平滑化範囲を広げるdef=5000
+            sigma_metric = 1  # 距離空間に関する標準偏差パラメータ  大きくすると色の平滑化範囲を広げる (d==0の時のみ有効)
+            img_bilateral_R = cv2.bilateralFilter(color_image_R, average_square_size, sigma_color,
+                                                  sigma_metric)  # Bilateralオペレータを使用して平滑化
+            img_bilateral_L = cv2.bilateralFilter(color_image_L, average_square_size, sigma_color,
+                                                  sigma_metric)  # Bilateralオペレータを使用して平滑化
+        hsv_img_R = cv2.cvtColor(img_bilateral_R, cv2.COLOR_BGR2HSV)  # HSVモデルに変更
+        img_gly_R = cv2.cvtColor(depth_colormap_R, cv2.COLOR_BGR2GRAY)  # グレースケール
+        hsv_img_L = cv2.cvtColor(img_bilateral_L, cv2.COLOR_BGR2HSV)  # HSVモデルに変更
+        img_gly_L = cv2.cvtColor(depth_colormap_L, cv2.COLOR_BGR2GRAY)  # グレースケール
 
         # 設定以上の段差検知すると、その部分を白塗する
-        white_color = 255  # RGBでの白色
+        white_color = 0  # RGBでの黒色
         depth_image_2d_R = np.dstack((depth_image_R, depth_image_R, depth_image_R))  # 配列同士を奥行きで重ねる。RGBに対してdepth情報追加
         bg_removed_R = np.where((depth_image_2d_R > clipping_distance) | (depth_image_2d_R < 0), depth_colormap_R, color_image_R)  # 引数1
         depth_image_2d_L = np.dstack((depth_image_L, depth_image_L, depth_image_L))  # 配列同士を奥行きで重ねる。RGBに対してdepth情報追加
         bg_removed_L = np.where((depth_image_2d_L > clipping_distance1) | (depth_image_2d_L < 0), depth_colormap_L, color_image_L)  # 引数1
 
+        # 2値処理
+        lower = (0, 7, 140)  # 色相は赤→黄→緑→水色→青→紫でループする
+        upper = (359, 255, 255)
+        bin_img_R = cv2.inRange(bg_removed_R, lower, upper)  # 2値化   引数1 画像指定  引数2 下限の色  引数3 上限の色
+        contours_R, hierarchy_R = cv2.findContours(bin_img_R, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 輪郭検出
+        contours_R = list(filter(lambda x: cv2.contourArea(x) > 10000, contours_R))  # 小さい輪郭は誤検出として削除する
+        bin_img_L = cv2.inRange(bg_removed_L, lower, upper)  # 2値化   引数1 画像指定  引数2 下限の色  引数3 上限の色
+        contours_L, hierarchy_L = cv2.findContours(bin_img_L, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 輪郭検出
+        contours_L = list(filter(lambda x: cv2.contourArea(x) > 10000, contours_L))  # 小さい輪郭は誤検出として削除する
+
         # Show images
         cv2.namedWindow('Right', cv2.WINDOW_AUTOSIZE)  # 条件式  #引数2 条件合致時の置き換え #引数3 条件不一致時の置き換え
-        cv2.imshow('Right', bg_removed_R)  # 輪郭処理
+        cv2.imshow('Right', bin_img_R)  # 輪郭処理
         cv2.namedWindow('Left', cv2.WINDOW_AUTOSIZE)  # 条件式  #引数2 条件合致時の置き換え #引数3 条件不一致時の置き換え
-        cv2.imshow('Left', bg_removed_L)  # 輪郭処理
+        cv2.imshow('Left', bin_img_L)  # 輪郭処理
 
+        xr = 440
+        xl = 200
+        yl1 = 10
+        yl2 = 30
         # 右側判定
-        if 255 != bg_removed_R[50:430, 0:170]:  # 前判定ゾーンで監視
+        if 0 == bin_img_R[yl1, xl].all() or 0 == bin_img_R[yl1, xr].all():  # 前判定ゾーンで監視
             dataR = 11  # 止まるモード
 
-        elif 255 != bg_removed_R[50:430, 170:640]:  # 左判定ゾーンで監視
+        elif 0 == bin_img_R[yl1, xr].all() or 0 == bin_img_R[yl2, xr].all():  # 引数1 y座標  引数2 x座標
+            dataR = 22  # 左モード
+
+        elif 0 == bin_img_R[yl1, xl].all() or 0 == bin_img_R[yl2, xl].all():  # 左判定ゾーンで監視
             dataR = 33  # "右モード
+
         else:
             dataR = 44  # 直進モード
 
         # 左側判定
-        if 255 != bg_removed_L[50:430, 470:640]:  # 前判定ゾーンで監視
+        if 0 == bin_img_L[yl1, xl].all() or 0 == bin_img_L[yl1, xr].all():  # 前判定ゾーンで監視
             dataL = 11  # 止まるモード
 
-        elif 255 != bg_removed_L[50:430, 0:470]:  # 引数1 y座標  引数2 x座標
+        elif 0 == bin_img_L[yl1, xr].all() or 0 == bin_img_L[yl2, xr].all():  # 引数1 y座標  引数2 x座標
             dataL = 22  # 左モード
+
+        elif 0 == bin_img_L[yl1, xl].all() or 0 == bin_img_L[yl2, xl].all():  # 左判定ゾーンで監視
+            dataL = 33  # 右モード
 
         else:
             dataL = 44  # 直進
 
         # 総合判定
-        if dataL == 11 and dataR == 11:
+        if dataR == 11 and dataL == 11:
             print("stop")
             data = 'stop'
 
-        elif dataL == 22:  # 引数1 y座標  引数2 x座標
+        elif dataR == 22 and dataL == 22:  # 引数1 y座標  引数2 x座標
             print("turn Left")
             data = 'turn Left'
 
-        elif dataR == 33:  # 左判定ゾーンで監視
-            print("turn Light")
-            data = 'turn Light'
+        elif dataR == 33 and dataL == 33:  # 左判定ゾーンで監視
+            print("turn Right")
+            data = 'turn Right'
 
         elif dataR == 44 and dataL == 44:
             print("keep")
@@ -239,4 +259,5 @@ try:
             cv2.destroyAllWindows()
             break
 finally:
+
     pipeline1.stop()
